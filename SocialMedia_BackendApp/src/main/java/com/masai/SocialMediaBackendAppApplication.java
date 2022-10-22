25a29d5292f4ff51292b808e0addbff7a19445cb
Nitish Kumar <103942404+nitish906@users.noreply.github.com>
2022-10-22 17:13:42 +0530

    social

---
+++ SocialMedia_BackendApp/src/main/java/com/masai/SocialMediaBackendAppApplication.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaBackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaBackendAppApplication.class, args);
+	}
+
+}
---
+++ SocialMedia_BackendApp/src/main/java/com/masai/model/UserLogin.java
@@ -0,0 +1,6 @@
+package com.masai.model;
+
+public class UserLogin {
+
+}
+//Due to lopTop issue i cannot finished the project. but leter i update the prject.pls
